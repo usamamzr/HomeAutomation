5ecf057b2ae17da47275935aa2a7fb306a2f3a52
usamamzr <usama.mzr96@gmail.com>
2018-12-05 20:56:38 +0500

    Laravel API interface

--- app/src/main/java/com/example/usama/homeautomation/API/LaravelAPI.java
+++ app/src/main/java/com/example/usama/homeautomation/API/LaravelAPI.java
@@ -1,6 +1,8 @@
 package com.example.usama.homeautomation.API;
 
 import com.example.usama.homeautomation.Models.Floor;
+import com.example.usama.homeautomation.Models.Room;
+import com.example.usama.homeautomation.Models.Thing;
 
 import java.util.ArrayList;
 
@@ -14,6 +16,9 @@ import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface LaravelAPI {
+
+    /////////////////   FLOORS  //////////////////////
+
     @GET("floors")
     Call<ArrayList<Floor>> getFloorsList();
 
@@ -23,15 +28,69 @@ public interface LaravelAPI {
     @POST("floors")
     @FormUrlEncoded
     Call<Floor> addFloors(@Field("id") String floorID,
-                         @Field("fName") String name,
+                          @Field("fName") String name,
                           @Field("Icon") String icon);
 
     @PUT("floors/{id}")
     @FormUrlEncoded
     Call<Floor> updateFloor(@Path("id") String floorID,
-                          @Field("fName") String name,
-                           @Field("Icon") String icon);
+                            @Field("fName") String name,
+                            @Field("Icon") String icon);
 
     @DELETE("floors/{id}")
     Call<String> deleteFloor(@Path("id") String floorID);
+
+    //////////////////////////////  ROOMS   //////////////////////////////////
+
+    @GET("rooms")
+    Call<ArrayList<Room>> getRoomsList();
+
+    @GET("rooms/{id}")
+    Call<Room> getSingleRoom();
+
+    @GET("rooms/ByfloorId/{floorid}")
+    Call<Room> getSingleRoomByFloor();
+
+    @POST("rooms")
+    @FormUrlEncoded
+    Call<Room> addRooms(@Field("id") String roomID,
+                         @Field("rName") String name,
+                         @Field("Icon") String icon,
+                         @Field("FloorId") String floorID);
+
+    @PUT("rooms/{id}")
+    @FormUrlEncoded
+    Call<Room> updateRoom(@Path("id") String roomID,
+                           @Field("rName") String name,
+                           @Field("Icon") String icon,
+                           @Field("FloorId") String floorID);
+
+    @DELETE("rooms/{id}")
+    Call<String> deleteRoom(@Path("id") String roomID);
+
+    //////////////////////////////  THINGS /////////////////////////////////
+
+    @GET("things")
+    Call<ArrayList<Thing>> getThingsList();
+
+    @GET("things/{id}")
+    Call<Thing> getSingleThing();
+
+    @GET("things/ByroomId/{id}")
+    Call<Thing> getSingleThingByRoom();
+
+    @POST("things")
+    @FormUrlEncoded
+    Call<Thing> addThings(@Field("id") String thingID,
+                         @Field("tName") String name,
+                         @Field("RoomId") String roomID);
+
+    @PUT("things/{id}")
+    @FormUrlEncoded
+    Call<Floor> updateThing(@Path("id") String thingID,
+                           @Field("tName") String name,
+                           @Field("RoomId") String roomID);
+
+    @DELETE("things/{id}")
+    Call<String> deleteThing(@Path("id") String thingID);
 }
