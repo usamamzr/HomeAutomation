47be0c4843a580e04e20e359a089c164e5fa83c7
usamamzr <usama.mzr96@gmail.com>
2018-12-05 16:13:30 +0500

    Recycler views implemented

---
+++ app/src/main/java/com/example/usama/homeautomation/Models/Floor.java
@@ -0,0 +1,25 @@
+package com.example.usama.homeautomation.Models;
+
+public class Floor {
+
+    String name;
+
+    @Override
+    public String toString() {
+        return "Floor{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public Floor(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
